3c22eee21bf9796033f49a542704e971dbe1be65
Shy <Shyamal@Ruparel.co>
2014-06-14 10:01:35 -0400

    Added directions to the side

--- static/maps.js
+++ static/maps.js
@@ -1,91 +1,79 @@
-var map;
 var directionsDisplay;
-var directionsService;
-var stepDisplay;
-var markerArray = [];
-var catdata = "";
+var directionsService = new google.maps.DirectionsService();
 
 function initialize() {
-  // Instantiate a directions service.
-  directionsService = new google.maps.DirectionsService();
+    directionsDisplay = new google.maps.DirectionsRenderer();
+    var manhattan = new google.maps.LatLng(40.7711329, -73.9741874);
+    var mapOptions = {
+        zoom: 13,
+        center: manhattan,
+        styles: [{
+            "stylers": [{
+                "saturation": -100
+            }]
+        }, {
+            "featureType": "water",
+            "elementType": "geometry.fill",
+            "stylers": [{
+                "color": "#0099dd"
+            }]
+        }, {
+            "elementType": "labels",
+            "stylers": [{
+                "visibility": "off"
+            }]
+        }, {
+            "featureType": "poi.park",
+            "elementType": "geometry.fill",
+            "stylers": [{
+                "color": "#aadd55"
+            }]
+        }, {
+            "featureType": "road.highway",
+            "elementType": "labels",
+            "stylers": [{
+                "visibility": "off"
+            }]
+        }, {
+            "featureType": "road.arterial",
+            "elementType": "labels.text",
+            "stylers": [{
+                "visibility": ""
+            }]
+        }, {
+            "featureType": "road.local",
+            "elementType": "labels.text",
+            "stylers": [{
+                "visibility": "on"
+            }]
+        }, {}]
+    };
 
-  // Create a map and center it on Manhattan.
-  var manhattan = new google.maps.LatLng(40.7711329, -73.9741874);
-  var mapOptions = {
-    zoom: 13,
-    center: manhattan,
-    styles: [{"stylers":[{"saturation":-100}]},{"featureType":"water","elementType":"geometry.fill","stylers":[{"color":"#0099dd"}]},{"elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"poi.park","elementType":"geometry.fill","stylers":[{"color":"#aadd55"}]},{"featureType":"road.highway","elementType":"labels","stylers":[{"visibility":"on"}]},{"featureType":"road.arterial","elementType":"labels.text","stylers":[{"visibility":"on"}]},{"featureType":"road.local","elementType":"labels.text","stylers":[{"visibility":"on"}]},{}]
-    // http://snazzymaps.com/?sort=recent
-  };
-  map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+    var map = new google.maps.Map(document.getElementById('map-canvas'),
+        mapOptions);
+    directionsDisplay.setMap(map);
+    directionsDisplay.setPanel(document.getElementById('directions-panel'));
 
-  // Create a renderer for directions and bind it to the map.
-  var rendererOptions = {
-    map: map
-  };
-  directionsDisplay = new google.maps.DirectionsRenderer(rendererOptions);
-
-  // Instantiate an info window to hold step text.
-  stepDisplay = new google.maps.InfoWindow();
+    var control = document.getElementById('control');
+    control.style.display = 'block';
+    map.controls[google.maps.ControlPosition.TOP_CENTER].push(control);
 }
 
 function calcRoute() {
+    var start = document.getElementById('start').value;
+    var end = document.getElementById('end').value;
+    var selectedMode = document.getElementById('mode').value;
+    var request = {
+        origin: start,
+        destination: end,
+        travelMode: google.maps.TravelMode[selectedMode]
 
-  // First, remove any existing markers from the map.
-  for (var i = 0; i < markerArray.length; i++) {
-    markerArray[i].setMap(null);
-  }
-
-  // Now, clear the array itself.
-  markerArray = [];
-
-  // Retrieve the start and end locations and create
-  // a DirectionsRequest using WALKING directions.
-  var start = document.getElementById('start').value;
-  var end = document.getElementById('end').value;
-  var selectedMode = document.getElementById('mode').value;
-  var request = {
-      origin: start,
-      destination: end,
-      travelMode: google.maps.TravelMode[selectedMode]
-  };
-
-  // Route the directions and pass the response to a
-  // function to create markers for each step.
-  directionsService.route(request, function(response, status) {
-    if (status == google.maps.DirectionsStatus.OK) {
-      var warnings = document.getElementById('warnings_panel');
-      warnings.innerHTML = '<b>' + response.routes[0].warnings + '</b>';
-      directionsDisplay.setDirections(response);
-      showSteps(response);
-    }
-  });
-}
-
-function showSteps(directionResult) {
-  // For each step, place a marker, and add the text to the marker's
-  // info window. Also attach the marker to an array so we
-  // can keep track of it and remove it when calculating new
-  // routes.
-  var myRoute = directionResult.routes[0].legs[0];
-
-  for (var i = 0; i < myRoute.steps.length; i++) {
-    var marker = new google.maps.Marker({
-      position: myRoute.steps[i].start_location,
-      map: map
+    };
+    directionsService.route(request, function (response, status) {
+        if (status == google.maps.DirectionsStatus.OK) {
+            directionsDisplay.setDirections(response);
+        }
     });
-    attachInstructionText(marker, myRoute.steps[i].instructions);
-    markerArray[i] = marker;
-  }
-}
-
-function attachInstructionText(marker, text) {
-  google.maps.event.addListener(marker, 'click', function() {
-    // Open an info window when the marker is clicked on,
-    // containing the text of the step.
-    stepDisplay.setContent(text);
-    stepDisplay.open(map, marker);
-  });
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
